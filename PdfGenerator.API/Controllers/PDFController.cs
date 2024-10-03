7f2d7545fdea1baa4b5c4abffe32df78def76cf1
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-10-03 11:41:50 +0500

    Add project files.

---
+++ PdfGenerator.API/Controllers/PDFController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelectPdf;
+
+namespace PdfGenerator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PDFController : ControllerBase
+    {
+        private readonly IPDFService _pdfService;
+
+        public PDFController(IPDFService pdfService)
+        {
+            _pdfService = pdfService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GeneratePDF(string html)
+        {
+            HtmlToPdf htmlToPdf = new HtmlToPdf();
+            PdfDocument doc = htmlToPdf.ConvertHtmlString(html);
+
+            doc.Save("C:\\Users\\Sardor\\Desktop\\a.pdf");
+
+            // PDF ni qaytarish
+            return Ok("PDF muvaffaqiyatli yaratildi");
+        }
+    }
+
+    public interface IPDFService
+    {
+        Task<byte[]> GeneratePDF(string html);
+    }
+
+    public class PDFService : IPDFService
+    {
+        public async Task<byte[]> GeneratePDF(string html)
+        {
+            return null;
+        }
+    }
+}
